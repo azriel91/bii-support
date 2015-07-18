5a46dd15c658144c33dd495943feff8341df5dee
azriel <azriel91@gmail.com>
2015-07-18 13:56:05 +1200

    Fixed cmake extraction script

--- travis/install/cmake.sh
+++ travis/install/cmake.sh
@@ -3,7 +3,7 @@ set -e
 
 SCRIPT_DIR=$(dirname $0)
 wget -O "${SCRIPT_DIR}/cmake-3.0.2-Linux-64.tar.gz" https://s3.amazonaws.com/biibinaries/thirdparty/cmake-3.0.2-Linux-64.tar.gz
-tar -xzf "${SCRIPT_DIR}/cmake-3.0.2-Linux-64.tar.gz"
+tar -xzf "${SCRIPT_DIR}/cmake-3.0.2-Linux-64.tar.gz" -C $SCRIPT_DIR
 sudo cp -fR "${SCRIPT_DIR}/cmake-3.0.2-Linux-64/*" /usr
 rm -rf "${SCRIPT_DIR}/cmake-3.0.2-Linux-64"
 rm "${SCRIPT_DIR}/cmake-3.0.2-Linux-64.tar.gz"
