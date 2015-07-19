ef54b19c310d3ad7e8f00492d0702cc0df94f201
azriel <azriel91@gmail.com>
2015-07-19 15:53:24 +1200

    Added repository for clang 3.6 which fixes chrono bug
    http://stackoverflow.com/questions/15747223/why-does-this-basic-thread-program-fail-with-clang-but-pass-in-g

--- travis/install/apt_update.sh
+++ travis/install/apt_update.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo add-apt-repository ppa:h-rayflood/llvm-upper # needed for clang 3.6
 sudo apt-get update -qq
--- travis/install/clang.sh
+++ travis/install/clang.sh
@@ -2,8 +2,10 @@
 set -ex
 
 if [ "$CXX" = "clang++" ]; then
-  sudo apt-get install -qq clang;
+  sudo apt-get install -qq clang-3.6;
   export CXX="clang++" CC="clang";
-  sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang 90;
-  sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 90;
+  sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang-3.6 90;
+  sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++-3.6 90;
+  sudo update-alternatives --install /usr/bin/clang cc /usr/bin/clang-3.6 90;
+  sudo update-alternatives --install /usr/bin/clang++ c++ /usr/bin/clang++-3.6 90;
 fi
