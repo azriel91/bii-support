1d7f91a9bf2173adb86c1d685318c8f054ca6479
azriel <azriel91@gmail.com>
2015-08-09 14:09:30 +1200

    Fixup refactor of scripts

--- linux/build/default.sh
+++ linux/build/default.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 set -e
 
-if [ -z $DRYRUN ]; then
+if [ -n $DRYRUN ]; then
   DRYRUN_REAL=echo
 fi
 
 SCRIPT_DIR=$(dirname $0)
 BLOCK_DIR="${SCRIPT_DIR}/../.."
 
-if [ "x${BUILD_SHARED_LIBS}x" != xx ] && [ "x${BUILD_SHARED_LIBS}x" != xONx ] && [ "x${BUILD_SHARED_LIBS}x" != xOFFx ]; then
+if [ -n $BUILD_SHARED_LIBS ] && [ "x${BUILD_SHARED_LIBS}x" != xONx ] && [ "x${BUILD_SHARED_LIBS}x" != xOFFx ]; then
   >&2 printf "The BUILD_SHARED_LIBS variable must be either blank, or set to ON or OFF\n"
   exit 1
 fi
@@ -21,7 +21,7 @@ if [ ! -f "${BLOCK_DIR}/bii/settings.bii" ]; then
 fi
 
 # Configure cmake if BUILD_SHARED_LIBS is specified
-if [ "x${BUILD_SHARED_LIBS}x" != xx ]; then
+if [ -n $BUILD_SHARED_LIBS ]; then
   $DRYRUN_REAL bii configure "-DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}"
 fi
 
