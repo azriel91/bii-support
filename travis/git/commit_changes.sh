4f7f0ef0c7a4b26b6f3380015395d665fdce5742
azriel <azriel91@gmail.com>
2015-08-09 15:14:43 +1200

    Added guard to commit_changes.sh
    This informs the user to ensure the git user is configured, and informs them to use the configure.sh script

--- travis/git/commit_changes.sh
+++ travis/git/commit_changes.sh
@@ -6,17 +6,22 @@ set -e
 # Protection if user has not defined environmental variables
 IS_MISSING_VARS=false
 
+if [ -z $(git config user.name) ] || [ -z $(git config user.email) ] ; then
+  >&2 printf "%s\n%s\n" \
+             "Please ensure that the git user.name and user.email are configured. "
+             "You may use the \"configure.sh\" script next to this script in the bii-support repository."
+  IS_MISSING_VARS=true
+fi
+
 if [ -z $GITHUB_TOKEN ]; then
   >&2 printf "%s\n%s\n%s\n" \
              "Please set the GITHUB_TOKEN secure environmental variable in .travis.yml:" \
-             "  - secure:=\"encrypted-personal-access-token\"" \
+             "  - secure: \"encrypted-personal-access-token\"" \
              "Follow the instructions at: http://blog.biicode.com/automatically-build-publish-via-travis-ci-github/"
   IS_MISSING_VARS=true
 fi
 
-if $IS_MISSING_VARS ; then
-  exit 1
-fi
+if $IS_MISSING_VARS ; then exit 1 fi
 
 # If there are changes, commit them
 git config credential.helper "store --file=.git/credentials" 
