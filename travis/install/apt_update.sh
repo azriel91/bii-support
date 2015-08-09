0abccaae39bc103b07d00ec3e42889ebf890ffe7
azriel <azriel91@gmail.com>
2015-08-09 15:56:52 +1200

    Bugfix: add -y when adding apt repository

--- travis/install/apt_update.sh
+++ travis/install/apt_update.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo add-apt-repository ppa:h-rayflood/llvm-upper # needed for clang 3.6
+sudo add-apt-repository ppa:h-rayflood/llvm-upper -y # needed for clang 3.6
 sudo apt-get update -qq
