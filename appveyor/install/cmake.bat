613c5845fb0a964cf2f154ff6a97fba7e6ccc68e
azriel <azriel91@gmail.com>
2015-10-07 06:07:26 +1300

    Use powershell to add to PATH variable

--- appveyor/install/cmake.bat
+++ appveyor/install/cmake.bat
@@ -1,15 +1,16 @@
 @echo off
 setlocal
 
+set SCRIPT_DIR=%~dp0
+set SCRIPT_DIR=%SCRIPT_DIR:~0,-1%
+
 echo "Downloading cmake"
 powershell "wget https://s3.amazonaws.com/biibinaries/thirdparty/cmake-3.0.2-win32-x86.zip -OutFile cmake.zip"
 
 echo "Unzipping cmake"
 7z x cmake.zip -o"C:\Program Files (x86)\" -y > nul
 
-:: 'set' sets the variable for the current batch script execution
-:: 'setx' sets it externally
 set PATH=%PATH%;C:\Program Files (x86)\cmake-3.0.2-win32-x86\bin
-setx PATH "%PATH%;C:\Program Files (x86)\cmake-3.0.2-win32-x86\bin"
+powershell -File "%SCRIPT_DIR%\internal\add_to_path.ps1" "C:\Program Files (x86)\cmake-3.0.2-win32-x86\bin"
 cmake --version
 del cmake.zip
